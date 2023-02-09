0737ec10dd44bb2f2d5a44230098f7e087edbf24
muhamedessam <muhamedessam96@hotmail.com>
2023-02-09 12:02:46 +0200

    - Create a unique ID for each note. - Use route model binding.

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -72,9 +72,12 @@ class NoteController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($uuid)
+    public function show(Note $note)
     {
-        $note = Note::where('uuid',$uuid)->where('user_id',Auth::id())->firstOrFail();
+        if($note->user_id != Auth::iD()){
+            return abort(403);
+        }
+        // $note = Note::where('uuid',$uuid)->where('user_id',Auth::id())->firstOrFail();
         return view('notes.show')->with('note',$note);
     }
 
