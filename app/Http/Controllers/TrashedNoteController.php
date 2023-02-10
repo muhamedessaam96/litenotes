dd003c67e25a6e41c38d7b4d2683feb4b8fe36a9
muhamedessam <muhamedessam96@hotmail.com>
2023-02-11 00:54:37 +0200

    - Show soft deleted model. - Restore soft deleted models.

--- app/Http/Controllers/TrashedNoteController.php
+++ app/Http/Controllers/TrashedNoteController.php
@@ -10,5 +10,25 @@ class TrashedNoteController extends Controller
 {
     public function index(){
         $notes = Note::whereBelongsTo(Auth::user())->onlyTrashed()->latest('updated_at')->paginate(5);
-        return view('notes.index')->with('notes',$notes);    }
+        return view('notes.index')->with('notes',$notes);
+    }
+
+    public function show(Note $note)
+    {
+        if(!$note->user->is(Auth::user())){
+            return abort(403);
+        }
+        return view('notes.show')->with('note',$note);
+    }
+
+    public function update(Note $note){
+
+        if(!$note->user->is(Auth::user())){
+            return abort(403);
+        }
+        $note->restore();
+
+        return to_route('notes.show',$note)->with('success','Note restored Successfully');
+    }
+
 }
--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Notes') }}
+            {{-- {{ request()->routeIs('notes.show') ? __('Notes') : __('Trash') }} --}}
+            {{-- OR --}}
+            {{ !$note->trashed() ? __('Notes') : __('Trash') }}
         </h2>
     </x-slot>
 
@@ -9,14 +11,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <x-alert-success>{{ session('success') }}</x-alert-success>
                 <div class="flex">
+                    @if (!$note->trashed())
                     <p class="opacity-70"><strong>Created :</strong>{{ $note->created_at->diffForHumans() }}</p>
-                    <p class="opacity-70 ml-8"><strong>Updated at- :</strong>{{ $note->updated_at->diffForHumans() }}</p>
+                    <p class="opacity-70 ml-8"><strong>Updated :</strong>{{ $note->updated_at->diffForHumans() }}</p>
                     <a href="{{ route('notes.edit',$note) }}" class="btn-link ml-auto">Edit Note</a>
                     <form action="{{ route('notes.destroy', $note) }}" method="post">
                         @method('delete')
                         @csrf
                         <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this form?')">Move to Trash</button>
                     </form>
+                    @else
+                    <p class="opacity-70"><strong>Deleted :</strong>{{ $note->deleted_at->diffForHumans() }}</p>
+                    <form action="{{ route('trashed.update', $note) }}" method="post" class="ml-auto">
+                    @method('put')
+                    @csrf
+                    <button type="submit" class="btn-link ">Restore Note</button>
+                    </form>
+                    {{-- <form action="{{ route('notes.destroy', $note) }}" method="post">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this form?')">Move to Trash</button>
+                    </form> --}}
+                    @endif
+
                 </div>
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-4xl">{{ $note->title }}</h2>
